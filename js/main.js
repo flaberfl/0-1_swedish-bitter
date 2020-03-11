a98ad62ca5db82660b590a30de40ca9534ce0c72
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-03-12 01:38:27 +0300

    home night

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function () {
 
-    $('.list__inner .tab').on('click', function (event) {
+    $('.list__inner .tab, .reviews__tab'/* Класс item ТАБА */).on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.list__inner').find('.tab-item').removeClass('active-tab').hide();
-        $('.list__inner .tabs').find('.tab').removeClass('active');
+        $('.list__inner, .reviews__inner'/* Общий родитель группы табов и контента! */).find('.tab-item, .reviews__content-item'/* Класс item Контента */).removeClass('active-tab').hide();
+        $('.list__inner .tabs, .reviews__tabs'/* Общий класс Табов */).find('.tab, .reviews__tab'/* Класс item ТАБА */).removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
