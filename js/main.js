1d18f0df5ecff144b30fd3043f77d93a161b8b4e
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-10-21 00:23:33 +0300

    завершены табы (функционал написан)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $(function () {
 
-    $('.list__inner .tab, .reviews__tab'/* Класс item ТАБА */).on('click', function (event) {
+    $('.list__inner .tab, .reviews__tab' /* Класс item ТАБА */ ).on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.list__inner, .reviews__inner'/* Общий родитель группы табов и контента! */).find('.tab-item, .reviews__content-item'/* Класс item Контента */).removeClass('active-tab').hide();
-        $('.list__inner .tabs, .reviews__tabs'/* Общий класс Табов */).find('.tab, .reviews__tab'/* Класс item ТАБА */).removeClass('active');
+        $('.list__inner, .reviews__inner' /* Общий родитель группы табов и контента! */ ).find('.tab-item, .reviews__content-item' /* Класс item Контента */ ).removeClass('active-tab').hide();
+        $('.list__inner .tabs, .reviews__tabs' /* Общий класс Табов */ ).find('.tab, .reviews__tab' /* Класс item ТАБА */ ).removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
-
-
 });
